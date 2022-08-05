6f21e0f94b4debaca47f9a0e3df0883b15af94ff
astra <diman.lan@Mail.ru>
2022-08-05 22:31:20 +0300

    First commit

---
+++ src/Book.java
@@ -0,0 +1,25 @@
+public class Book {
+    private final String name;
+    public Author author;
+    private int publishDate;
+
+    public Book(String name, Author author, int publishDate) {
+        this.name = name;
+        this.author = author;
+        this.publishDate = publishDate;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+    public int getPublishDate() {
+        return this.publishDate;
+    }
+    public void setPublishDate(int publishDate) {
+        this.publishDate = publishDate;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author author = new Author("Брэм", "Стокер");
+        Book book = new Book("Дракула", author, 1888);
+        System.out.println("Название: " + book.getName() + "; Автор: " + book.getAuthor() + "; Дата публикации: " + book.getPublishDate());
+        book.setPublishDate(1897);
+        System.out.println("Дата публикации через сеттер = " + book.getPublishDate());
+
+        Author author1 = new Author("Лев", "Толстой");
+        Book book1 = new Book("Война и мир", author1, 1767);
+        System.out.println("Название: " + book1.getName() + "; Автор: " + book1.getAuthor() + "; Дата публикации: " + book1.getPublishDate());
+        book1.setPublishDate(1867);
+        System.out.println("Дата публикации через сеттер = " + book1.getPublishDate());
     }
 }
